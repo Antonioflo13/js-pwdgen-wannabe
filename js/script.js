c9251c2c77fbc8fa03409545b35cf1e4f2b072d7
Antonioflo13 <floreantonio@hotmail.it>
2021-04-26 16:27:38 +0200

    first .js with style

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var nome = prompt ("Ciao, qual'è il tuo nome?");
+var cognome = prompt ("Ciao, qual'è il tuo cognome?");
+var colore = prompt ("Qual'è il tuo colore preferito?");
+document.getElementById ("password") .innerHTML = (nome + cognome + colore + "21");
